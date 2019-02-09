b38c8e98ae430efb90dcb2063464c37c94771dec
ridhodarman <33389796+ridhodarman@users.noreply.github.com>
2019-02-09 13:54:56 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php
 
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "root";
+	$host = "ec2-54-235-67-106.compute-1.amazonaws.com";
+	$user = "scehnwetxoamzm";
+	$pass = "075d5ceeef1b9c73d0830b65e30b44530d136ba1b0ec42a2bc6fd43f0019b8a7";
 	$port = "5432";
-	$dbname = "test_a";
+	$dbname = "d80cvv3nckfpri";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 
 ?>
